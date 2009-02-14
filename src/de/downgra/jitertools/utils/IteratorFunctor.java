b13a1f4c5b2231043e5be63c9f2ba86118fa9d8c
Rico Schiekel <fire@downgra.de>
2009-02-15 00:52:16 +0100

    add special functor which converts an iterable to an iterator.

---
+++ src/de/downgra/jitertools/utils/IteratorFunctor.java
@@ -0,0 +1,11 @@
+package de.downgra.jitertools.utils;
+
+import java.util.Iterator;
+
+public class IteratorFunctor<T> implements IFunctor<Iterator<T>, Iterable<T>> {
+	@Override
+	public Iterator<T> call(Iterable<T> object) {
+		return object.iterator();
+	}
+
+}
