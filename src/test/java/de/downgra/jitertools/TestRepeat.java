3a8ba15614fa209f9f9d94485b098d2d731d6afd
Rico Schiekel <fire@downgra.de>
2009-02-16 14:35:50 +0100

    some code formating changes

--- src/test/java/de/downgra/jitertools/TestRepeat.java
+++ src/test/java/de/downgra/jitertools/TestRepeat.java
@@ -1,7 +1,7 @@
 package de.downgra.jitertools;
 
-import static de.downgra.jitertools.JIterTools.repeat;
 import static de.downgra.jitertools.JIterTools.list;
+import static de.downgra.jitertools.JIterTools.repeat;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
@@ -9,20 +9,21 @@ import org.junit.Test;
 
 public class TestRepeat {
 
-    @Test
-    public void repeatInfinite() {
-        Integer value = new Integer(23);
-        int _max = 1000;
-        for (final Integer item: repeat(value)) {
-            assertEquals(item, value);
-            if(_max-- <= 0) break;
-        }
-    }
+	@Test
+	public void repeatInfinite() {
+		Integer value = new Integer(23);
+		int _max = 1000;
+		for (final Integer item : repeat(value)) {
+			assertEquals(item, value);
+			if (_max-- <= 0)
+				break;
+		}
+	}
 
 	@Test
 	public void repeatTimes() {
-		assertArrayEquals(new Integer[] { 42, 42, 42, 42, 42 }, 
-                list(repeat(new Integer(42), 5)).toArray(new Integer[0]));
+		assertArrayEquals(new Integer[] { 42, 42, 42, 42, 42 }, list(
+				repeat(new Integer(42), 5)).toArray(new Integer[0]));
 	}
 
 }
