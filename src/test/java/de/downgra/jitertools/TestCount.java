bb25e536471264e53e4c6e4320f1098288aeaa51
Rico Schiekel <fire@downgra.de>
2009-02-17 14:41:23 +0100

    refactor count test

--- src/test/java/de/downgra/jitertools/TestCount.java
+++ src/test/java/de/downgra/jitertools/TestCount.java
@@ -1,5 +1,6 @@
 package de.downgra.jitertools;
 
+import static de.downgra.jitertools.JIterTools.count;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -9,7 +10,7 @@ public class TestCount {
 	@Test
 	public void normal() {
 		int validator = 0;
-		for (final Integer i : JIterTools.count()) {
+		for (final Integer i : count()) {
 			assertEquals(validator++, i);
 			if (i > 10)
 				break;
@@ -19,7 +20,7 @@ public class TestCount {
 	@Test
 	public void startValue() {
 		int validator = 1234;
-		for (final Integer i : JIterTools.count(validator)) {
+		for (final Integer i : count(validator)) {
 			assertEquals(validator++, i);
 			if (i > 1234 + 10)
 				break;
